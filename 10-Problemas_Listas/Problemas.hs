a946543046661ec7f4976e81f8411f84dcbd18fd
Canek García <kaan.ek@ciencias.unam.mx>
2023-10-13 00:07:09 -0600

    61.Problema3

--- 10-Problemas_Listas/Problemas.hs
+++ 10-Problemas_Listas/Problemas.hs
@@ -5,3 +5,13 @@ ones = repeat 1
 ---- 60. Problema 2
 nats :: [Integer]
 nats = iterate (+1) 0
+
+---- 61. Problema 3
+ints :: [Integer]
+ints = iterate integers 0
+    where
+    integers :: Integer -> Integer
+    integers x
+        | x > 0 = -x
+        | otherwise = 1 - x
+
