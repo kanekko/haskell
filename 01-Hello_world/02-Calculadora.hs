efe237039a1f35580a4c825a74b2685c3604d4d5
Canek García <kaan.ek@ciencias.unam.mx>
2024-08-09 11:22:46 -0600

    Added examples

---
+++ 01-Hello_world/02-Calculadora.hs
@@ -0,0 +1,22 @@
+sumar :: Integer -> Integer -> Integer
+sumar x y = x + y
+
+restar :: Integer -> Integer -> Integer
+restar x y = x - y
+
+multiplicar :: Integer -> Integer -> Integer
+multiplicar x y = x * y
+
+dividir :: Integer -> Integer -> Integer
+dividir x y = div x y
+
+
+doble :: Integer -> Integer
+doble x = 2 * x
+
+cuadrado :: Integer -> Integer
+cuadrado x = x * x
+
+
+parimetro :: Integer -> Integer -> Integer
+parimetro base altura = doble (base + altura)
