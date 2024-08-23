7a2f36afddb41caf5500e17f7ec6e53cd54d03e4
Canek García <kaan.ek@ciencias.unam.mx>
2024-08-23 11:39:53 -0600

    Updated examples

--- 01-Introducción/04-DA_Data.hs
+++ 01-Introducción/04-DA_Data.hs
@@ -19,10 +19,23 @@ lados Cuadrado = 4
 
 
 {-
-Ejemplo 3
+Ejemplo 3a
+
+data Planeta = Mercurio | Venus | Tierra | Marte |
+               Jupiter | Saturno | Urano | Neptuno
+
+habitable :: Planeta -> Bool
+habitable Tierra = True
+habitable _      = False
+-}
+
+
+{-
+Ejemplo 3b
 -}
 data Planeta = Mercurio | Venus | Tierra | Marte |
                Jupiter | Saturno | Urano | Neptuno
+               deriving (Eq, Ord, Show, Enum)
 
 habitable :: Planeta -> Bool
 habitable Tierra = True
--- 01-Introducción/05-DA_Type.hs
+++ 01-Introducción/05-DA_Type.hs
@@ -13,4 +13,31 @@ type NombreApellido  = String
 type Edad    = Int
 type Salario = Float
 
-data Trabajador = Alta NombreApellido Edad Salario | Baja NombreApellido
+data Trabajador = Interno NombreApellido Edad Salario | Externo NombreApellido
+
+{-
+Ejemplo 3
+-}
+type NombreCompleto  = String
+type SalarioNuevo = Float
+type Empresa = String
+
+data Empleado = EmpleadoInterno NombreCompleto SalarioNuevo | EmpleadoExterno NombreCompleto Empresa
+
+showEmpleado :: Empleado -> String
+showEmpleado (EmpleadoInterno n s) = n ++ " gana $" ++ (show s)
+showEmpleado (EmpleadoExterno n e) = n ++ " es de la empresa " ++ e
+
+
+{-
+Ejemplo 4
+-}
+data AExp = Lit Int | Add AExp AExp | Mul AExp AExp
+
+eval :: AExp -> Int
+eval (Lit x) = x
+
+evalrec :: AExp -> Int
+evalrec (Lit x)   = eval (Lit x)
+evalrec (Add x y) = (evalrec x) + (evalrec y)
+evalrec (Mul x y) = (evalrec x) * (evalrec y)
